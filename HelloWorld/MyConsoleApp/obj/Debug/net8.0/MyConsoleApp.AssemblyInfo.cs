957644be782c4a2a95c369d7e5a2b3cf66350530
keelw <wjordankeel@gmail.com>
2024-06-22 14:07:04 -0600

    hello world complete

--- HelloWorld/MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ HelloWorld/MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f027f0e4571f835c368b56ee4a24ba3bb9d5476a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+379d58feabbc1234472282b26699d3e15a118e00")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

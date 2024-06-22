c6534cbde0c4e53708f383c30d9a3cbe8ec5c31b
keelw <wjordankeel@gmail.com>
2024-06-22 12:43:01 -0600

    initializing file project

--- HelloWorld/MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ HelloWorld/MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a1a69a19c935586858e452bdda0b77b62fc5e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+016498e896346cc31aa3eeb0ffdd27ef6a94bfad")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

016498e896346cc31aa3eeb0ffdd27ef6a94bfad
keelw <wjordankeel@gmail.com>
2024-06-21 22:27:52 -0600

    debugging

--- HelloWorld/MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ HelloWorld/MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81045dcefdaa60ec9cce2670c47f1d9dc94204d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a1a69a19c935586858e452bdda0b77b62fc5e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
